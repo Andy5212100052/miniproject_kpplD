780c49ab88e2e6d70245de7ca7e7d8ef0450e8f7
Andy5212100052 <Andy5212100052@users.noreply.github.com>
2015-04-02 17:59:36 +0700

    Create posting2.php

---
+++ posting2.php
@@ -0,0 +1,22 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "kppl_andy";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	$username = $_POST['usernm'];
+	$posting = $_POST['new'];
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+		header("location:profile.php?name=".$username."");
+	}
+	
+	$sql = "INSERT INTO `posting`(`username`, `post`) VALUES ('".$username."','".$posting."')";
+	
+	$conn->query($sql);
+	header("location:profile.php?name=".$username."");
+	
+	mysqli_close($conn);							
+?>

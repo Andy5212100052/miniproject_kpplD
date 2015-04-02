f3ff233e6df4ff49a9b54a3f22dd35c37e513215
Andy5212100052 <Andy5212100052@users.noreply.github.com>
2015-04-02 17:56:59 +0700

    Create like.php

---
+++ like.php
@@ -0,0 +1,26 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "kppl_andy";
+	
+	$name = $_GET['name'];
+	$id = $_GET['id'];
+	
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	$sql = 	"UPDATE `motion` SET liked = liked + 1 WHERE M_ID = '".$id."'";
+	
+	if ($conn->query($sql) === TRUE) {
+		header("location:gerakan3.php?name=".$name."&id=".$id);
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	$conn->close();
+
+?>
